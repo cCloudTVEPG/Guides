8c22dd270c0c5656fce0e20f4e967ac6396b6335
cCloudTVEPG <45474462+cCloudTVEPG@users.noreply.github.com>
2018-12-08 16:50:00 -0700

    Daily EPG commit: All Channels12/08/2018

--- upload.bat
+++
@@ -1,8 +0,0 @@
-
-d:
-cd Git\cCloud
-git add --all
-
-git commit -am "Regular auto-commit %date:~-10,2%/%date:~-7,2%/%date:~-4,4% 
-git push
-PAUSE
--- upload2.bat
+++
@@ -1,7 +0,0 @@
-
-git add .
-git commit -am "Third day of batch automation successfull"
-git push 
-
-
-PAUSE
