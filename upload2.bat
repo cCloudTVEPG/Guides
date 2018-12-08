3376c94e645056003caaefc5c9e5c59aa2cd38d6
cCloudTVEPG <45474462+cCloudTVEPG@users.noreply.github.com>
2018-12-07 23:08:24 -0700

    Regular auto-commit $(timestamp)

--- upload2.bat
+++ upload2.bat
@@ -2,7 +2,7 @@
 cd path/to/your/project
 git add --all
 timestamp() {
-  _%date:~-4,4%%date:~-7,2%%date:~-10,2%
+  %date:~-4,4%%date:~-7,2%%date:~-10,2%
 }
 git commit -am "Regular auto-commit $(timestamp)"
 git push
