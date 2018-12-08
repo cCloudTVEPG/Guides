6eec6323db1c0002e04f30793d97ba5afde7fb37
cCloudTVEPG <45474462+cCloudTVEPG@users.noreply.github.com>
2018-12-07 23:27:17 -0700

    Regular auto-commit $(_20180712 at M:S

--- upload2.bat
+++ upload2.bat
@@ -2,7 +2,7 @@
 d:
 cd Git\cCloud
 git add --all
-timestamp(_%date:~-4,4%%date:~-7,2%%date:~-10,2%) {_%date:~-4,4%%date:~-7,2%%date:~-10,2%}
-git commit -am "Regular auto-commit $(_%date:~-4,4%%date:~-7,2%%date:~-10,2%"
+
+git commit -am "Regular auto-commit $(_%date:~-4,4%%date:~-7,2%%date:~-10,2% at %H:%M:%S"
 git push
 PAUSE
